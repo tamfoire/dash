92c6eadc698075882d9e4091434b745b744eb1a5
tamfoire <114654009+tamfoire@users.noreply.github.com>
2023-04-25 17:38:35 +0100

    Add files via upload

---
+++ dash/orders.js
@@ -0,0 +1,49 @@
+const Orders = [
+    {
+        accountName: 'Awacash Mfb',
+        accountNumber: '8563111111',
+        paymentStatus: 'Due',
+        transaction: 'Pending'
+    },
+    {
+        accountName: 'Access Mfb',
+        accountNumber: '8563111111',
+        paymentStatus: 'Refunded',
+        transaction: 'Declined'
+    },
+    {
+        accountName: 'Fidelty Mfb',
+        accountNumber: '8563111111',
+        paymentStatus: 'Due',
+        transaction: 'Pending'
+    },
+    {
+        accountName: 'Uba Mfb',
+        accountNumber: '8563111111',
+        paymentStatus: 'Paid',
+        transaction: 'Sucessful'
+    },
+    {
+        accountName: 'First Mfb',
+        accountNumber: '8563111111',
+        paymentStatus: 'Paid',
+        transaction: 'Sucessful'
+    },
+    {
+        accountName: 'Zenith Mfb',
+        accountNumber: '8563111111',
+        paymentStatus: 'Due',
+        transaction: 'Pending'
+    },
+    {
+        accountName: 'Quilox Mfb',
+        acccountNumber: '8563111111',
+        paymentStatus: 'Paid',
+        transaction: 'Sucessful'
+    }
+]
+                            
+                            
+                            
+                            
+                            
